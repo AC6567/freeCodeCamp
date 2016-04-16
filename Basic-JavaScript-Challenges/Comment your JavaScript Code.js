2cbb5f911a6363ee87f55cbd088b320b3d4f79d0
PClark <BlackGuyCoding@users.noreply.github.com>
2016-04-16 12:03:03 +0300

    Create Comment your JavaScript Code.js

---
+++ Basic-JavaScript-Challenges/Comment your JavaScript Code.js
@@ -0,0 +1,11 @@
+
+/* Challenge #1: Comment your JavaScript Code 
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.freecodecamp.com/blackguycoding
+*/
+
+/* This is a 
+   multi-line comment */
+
+// This is an in-line comment.
