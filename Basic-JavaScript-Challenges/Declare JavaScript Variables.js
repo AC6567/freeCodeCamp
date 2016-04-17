f86433ede24155cd5e7ea54da5141f64a328ca96
PClark <BlackGuyCoding@users.noreply.github.com>
2016-04-17 13:11:36 +0300

    Create Declare JavaScript Variables.js

---
+++ Basic-JavaScript-Challenges/Declare JavaScript Variables.js
@@ -0,0 +1,12 @@
+/* Challenge #2: Declare JavaScript Variables
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.freecodecamp.com/blackguycoding
+*/
+
+// Example
+var ourName;
+
+// Define myName below this line
+var myName;
+
