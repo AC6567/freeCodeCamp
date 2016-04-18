f1e1aa5296c4123363ee96462f7b79d74d67e6e4
Perrin Clark <blackguycoding@gmail.com>
2016-04-18 09:25:16 +0300

    create new javascript solution

---
+++ Basic-JavaScript-Challenges/31: Use Bracket Notation to Find the First Character in a String.js
@@ -0,0 +1,17 @@
+/* Challenge #31: Use Bracket Notation to Find the First Character in a String
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.freecodecamp.com/blackguycoding
+*/
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
