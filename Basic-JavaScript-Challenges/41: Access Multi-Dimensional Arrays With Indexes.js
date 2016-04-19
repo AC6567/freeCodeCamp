dbdce8a8a741bcd767d93c960b7723b2405861fd
Perrin Clark <blackguycoding@gmail.com>
2016-04-19 20:53:26 +0300

    create new solutions

--- Basic-JavaScript-Challenges/41: Access Multi-Dimensional Arrays With Indexes.js
+++ Basic-JavaScript-Challenges/41: Access Multi-Dimensional Arrays With Indexes.js
@@ -3,3 +3,8 @@ Code by Perrin Clark
 BlackGuyCoding@gmail.com
 https://www.freecodecamp.com/blackguycoding
 */
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
---
+++ Basic-JavaScript-Challenges/44: Manipulate Arrays With shift.js
@@ -0,0 +1,15 @@
+/* Challenge #44: Manipulate Arrays With shift
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.freecodecamp.com/blackguycoding
+*/
+// Example
+var ourArray = ["Stimpson", "J", ["cat"]];
+removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.shift();
