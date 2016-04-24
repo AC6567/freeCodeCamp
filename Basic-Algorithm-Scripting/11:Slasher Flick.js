04b81a886aa132fdbdd13d28169103c354caa25a
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 13:39:33 +0300

    new Basic Algorithm solution

---
+++ Basic-Algorithm-Scripting/11:Slasher Flick.js
@@ -0,0 +1,13 @@
+/* Challenge #11: Slasher Flick
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.freecodecamp.com/blackguycoding */
+
+function slasher(arr, howMany) {
+  var temp = arr.slice(howMany);
+
+
+  return temp;
+}
+
+slasher([1, 2, 3], 2);
