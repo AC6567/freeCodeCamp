51ed3b7e4aeae994aa53cdc264562849d6fef4a2
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 14:06:08 +0300

    created intermediate algorithm scripting solution

---
+++ Intermediate-Algorithm-Scripting/19:Binary Agents.js
@@ -0,0 +1,16 @@
+/* Challenge #19: Binary Agents
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.freecodecamp.com/blackguycoding */
+
+function binaryAgent(str) {
+  var arr = str.split(" ");
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = parseInt(arr[i], 2).toString(10);
+  }
+  return arr.map(function(val) {
+    return String.fromCharCode(parseInt(val));
+  }).join("");
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
