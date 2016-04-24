fc35f2fc632dd1ec207f1fab36de67a41bb2c386
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 14:05:34 +0300

    created intermediate algorithm scripting solution

---
+++ Intermediate-Algorithm-Scripting/9:Boo who.js
@@ -0,0 +1,10 @@
+/* Challenge #9: Boo who
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.freecodecamp.com/blackguycoding */
+
+function booWho(bool) {
+  return bool === true || bool === false;
+}
+
+booWho(null);
