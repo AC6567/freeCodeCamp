b1726691e27256b40807626b75d6730801254982
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 13:22:18 +0300

    added Basic Algorithm solution

---
+++ Basic-Algorithm-Scripting/1:Reverse a String.js
@@ -0,0 +1,10 @@
+/* Challenge #1: Reverse a String
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.freecodecamp.com/blackguycoding */
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+reverseString("hello");
